cd516c3687ae515d39c94d01f54ce198b62dc0bd
= <=>
2020-11-24 10:30:00 -0300

    Adicionado rotas na aplicação

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import routes from './shared/router/main.router';
+import { createRouter, createWebHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
